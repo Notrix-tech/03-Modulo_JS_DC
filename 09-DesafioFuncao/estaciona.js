e04d20f872fbc4b1deb7c3f1070ecb828662a9bf
Clairtonlm <clairtonlm@gmail.com>
2023-01-27 21:22:49 -0300

    Controle de Entrada de veiculos

---
+++ 09-DesafioFuncao/estaciona.js
@@ -0,0 +1,30 @@
+//
+let manha = 0
+let tarde = 0
+let noite = 0
+function checarHorario(hora){
+    if(hora >= "05:00" && hora <= "11:59"){
+        manha++
+        return;
+    }
+    if(hora >= "12:00" && hora <= "17:59"){
+        tarde++
+        return;
+    }
+    if((hora >= "18:00" && hora <= "23:59") || (hora >= "00:00" && hora <= "04:59")){
+        noite++
+        return;
+    }
+}
+
+checarHorario("12:00")
+checarHorario("11:00")
+checarHorario("09:00")
+checarHorario("10:00")
+checarHorario("12:45")
+checarHorario("14:56")
+checarHorario("05:40")
+checarHorario("19:00")
+checarHorario("18:30")
+
+document.write(` Acessos da manha: ${manha}<br> Acessos da tarde: ${tarde}<br>Acessos da noite: ${noite}`)
