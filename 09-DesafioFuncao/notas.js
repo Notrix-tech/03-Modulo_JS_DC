abbe047e50e5a99dc75264c6972fa2473334298b
Clairtonlm <clairtonlm@gmail.com>
2023-01-27 21:10:22 -0300

    Notas no input

---
+++ 09-DesafioFuncao/notas.js
@@ -0,0 +1,6 @@
+//
+let botao = document.getElementById("exc")
+botao.addEventListener("click", function(evento){
+    evento.preventDefault()
+    documento.write(" A médoa é " + mediaPonderada(Number(n1.value),Number(n2.value), Number(n3.value)))
+})
