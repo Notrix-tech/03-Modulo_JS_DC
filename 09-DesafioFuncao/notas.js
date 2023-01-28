59dcfd93b520c6666fa72538a0759f6fe0b750b1
Clairtonlm <clairtonlm@gmail.com>
2023-01-28 12:05:06 -0300

    Pegando valor do Input

--- 09-DesafioFuncao/notas.js
+++ 09-DesafioFuncao/notas.js
@@ -2,5 +2,13 @@
 let botao = document.getElementById("exc")
 botao.addEventListener("click", function(evento){
     evento.preventDefault()
-    documento.write(" A médoa é " + mediaPonderada(Number(n1.value),Number(n2.value), Number(n3.value)))
+    documento.write(" A média é " + mediaPonderada(Number(n1.value),Number(n2.value), Number(n3.value)))
 })
+
+function mediaPonderada(t1, p1, p2){
+    let media = ((t1*2 + p1 + p2))/4;
+    return ` A media do aluno é ${media.toFixed(2)}`;
+
+}
+mediaPonderada(8,8,8)
+console.log(mediaPonderada(8,8,8))
