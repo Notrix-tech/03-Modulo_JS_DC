88ad65a9cc3f5dabc2b4bdfc1e3e11f1db288d58
Clairtonlm <clairtonlm@gmail.com>
2023-03-10 21:43:19 -0300

    Criando Editar

--- 15-CadastroAlunos/script.js
+++ 15-CadastroAlunos/script.js
@@ -43,7 +43,13 @@ function listarAlunos(){
                                <td>${lista[i].telefone}</td>
                                <td>${lista[i].cidade}</td>
                                <td>
-                                    <button class="btn btn-success">Editar</button>
+                                    <button 
+                                    type="button"
+                                    data-bs-toggle="offcanvas" 
+                                    data-bs-target="#offcanvasRight_editar"
+                                    aria-controls="offcanvasRight_editar"
+                                    class="btn btn-success"
+                                    onclik="preencherForm(${i})">Editar</button>
                                     <button class="btn btn-danger">Deletar</button>
                                </td>
         
@@ -61,8 +67,33 @@ function adicionarAluno(){
         cidade: cidade.value,
 
     }
-    //2passo- Inserir o objeto dentro do arry lista .push
+    
     lista.push(aluno);
+    cadastro.reset();
+    document.querySelector(".offcanvas").classList.remove("show")
+    document.querySelector(".offcanvas-backdrop").classList.remove("show")
+    
+    listarAlunos()
+}
+function preencherForm(posicao){
+    let aluno = lista[posicao];
+    e_id.value = posicao;
+    e_nome.value = aluno.nome;
+    e_email.value = aluno.email;
+    e_telefone.value = aluno.telefone;
+    e_cidade.value = aluno.cidade;
+}
+
+function editarAluno(){
+    event.preventDefault();
+    let posicao = e_id.value;
+    lista[posicao].nome = e_nome.value;
+    lista[posicao].email = e_email.value;
+    lista[posicao].telefone = e_telefone.value;
+    lista[posicao].cidade = e_cidade.value;
+    editar.reset();
+    document.querySelector(".offcanvas").classList.remove("show")
+    document.querySelector(".offcanvas-backdrop").classList.remove("show")
     //3passo- Carregar a Lista
     listarAlunos()//5passo-chama de novo
 }
