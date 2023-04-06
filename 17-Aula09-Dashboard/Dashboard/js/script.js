3b78949cd1591511cf3e30de6192cb7fc1dc53a3
Clairtonlm <clairtonlm@gmail.com>
2023-04-05 23:18:09 -0300

    Iserindo modo Dark

--- 17-Aula09-Dashboard/Dashboard/js/script.js
+++ 17-Aula09-Dashboard/Dashboard/js/script.js
@@ -1,4 +1,10 @@
-let troca = document.querySelector(".troca");
+let troca = document.querySelector(".troca");//mudando para dark
+let body = document.querySelector("body");//dark
+let header = document.querySelector("header")//encolhendo
+let main = document.querySelector("main")
 troca.addEventListener("click", ()=>{
     troca.classList.toggle("active");
+    body.classList.toggle("dark")
+    header.classList.toggle("active")
+    main.classList.toggle("active")
 })
